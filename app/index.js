1883f2535633a6e795972e1d879123fc88e7263b
gitfilix <runlevel07@gmail.com>
2019-04-24 17:23:11 +0200

    destructing objects and arrays

--- app/index.js
+++ app/index.js
@@ -1,4 +1,15 @@
-// template strings
-let birthday = `happy birthday`;
-let greetings = `${birthday} mr. president`;
-console.log("the template string is: ", greetings);
+// destructuring on arrays
+let animals = ['simba', 'sumsi', 'frank', 'penny'];
+let [dog, bird] = animals;
+
+console.log(dog, bird);
+
+// destructuring assignements on objects
+let king = {
+    name: 'Lionbaby King',
+    kids: 2,
+    hobbies: 'hunting, lying arround'
+}
+let {name, hobbies} = king;
+
+console.log("from an object", name, hobbies);
